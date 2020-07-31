fac42f2454f990fa081bf6b4fd4f4fef48557de5
Xlorgeksidin <65675106+Xlorgeksidin@users.noreply.github.com>
2020-07-31 15:57:53 +0400

    Улучшен метод заполнения формы контакта. Уменьшено время ожидания до 2 секунд

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -2,6 +2,7 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactHelper extends HelperBase{
@@ -10,7 +11,7 @@ public class ContactHelper extends HelperBase{
     super(wd);
   }
 
-  public void fillContactCreateForm(ContactData contactData) {
+  public void fillContactForm(boolean creation, ContactData contactData) {
     type(By.name("firstname"), contactData.getFirstName());
     type(By.name("middlename"), contactData.getMiddleName());
     type(By.name("lastname"), contactData.getLastName());
@@ -32,34 +33,12 @@ public class ContactHelper extends HelperBase{
     selectElementFromList(By.name("aday"), contactData.getDayOfAnniversary());
     selectElementFromList(By.name("amonth"), contactData.getMonthOfAnniversary());
     type(By.name("ayear"), contactData.getYearOfAnniversary());
-    selectElementFromList(By.name("new_group"), contactData.getGroupName());
-    type(By.name("address2"), contactData.getAddress2());
-    type(By.name("phone2"), contactData.getPhone2());
-    type(By.name("notes"), contactData.getNotes());
-  }
 
-  public void fillContactModificationForm(ContactData contactData) {
-    type(By.name("firstname"), contactData.getFirstName());
-    type(By.name("middlename"), contactData.getMiddleName());
-    type(By.name("lastname"), contactData.getLastName());
-    type(By.name("nickname"), contactData.getNickname());
-    type(By.name("title"), contactData.getTitle());
-    type(By.name("company"), contactData.getCompany());
-    type(By.name("address"), contactData.getAddress());
-    type(By.name("home"), contactData.getHomeTelephone());
-    type(By.name("mobile"), contactData.getMobileTelephone());
-    type(By.name("work"), contactData.getWorkTelephone());
-    type(By.name("fax"), contactData.getFaxTelephone());
-    type(By.name("email"), contactData.getEmail());
-    type(By.name("email2"), contactData.getEmail2());
-    type(By.name("email3"), contactData.getEmail3());
-    type(By.name("homepage"), contactData.getHomepage());
-    selectElementFromList(By.name("bday"), contactData.getDayOfBirth());
-    selectElementFromList(By.name("bmonth"), contactData.getMonthOfBirth());
-    type(By.name("byear"), contactData.getYearOfBirth());
-    selectElementFromList(By.name("aday"), contactData.getDayOfAnniversary());
-    selectElementFromList(By.name("amonth"), contactData.getMonthOfAnniversary());
-    type(By.name("ayear"), contactData.getYearOfAnniversary());
+    if(creation){
+      selectElementFromList(By.name("new_group"), contactData.getGroupName());
+    } else{
+      Assert.assertFalse(isElementPresent(By.name("new_group")));
+    }
 
     type(By.name("address2"), contactData.getAddress2());
     type(By.name("phone2"), contactData.getPhone2());
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
@@ -9,7 +9,7 @@ public class ContactModificationTest extends TestBase{
   public void testContactModification(){
 
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillContactModificationForm(new ContactData("efirstName", "eMiddleName", "eLastName", "nickname", "title", "company", "address","home", "mobile", "work", "fax", "email", "email2", "email3", "homepage", "1", "January", "2001", "6", "August", "1993", "edit1", "address2", "phone2", "notes"));
+    app.getContactHelper().fillContactForm(false, new ContactData("firstName", "MiddleName", "LastName", "nickname", "title", "company", "address","home", "mobile", "work", "fax", "email", "email2", "email3", "homepage", "1", "January", "2001", "6", "August", "1993", null, "address2", "phone2", "notes"));
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToHomePage();
   }
