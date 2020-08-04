347ed8c1c2e3583f0ebb97da74ac4d1e72d297cf
Xlorgeksidin <65675106+Xlorgeksidin@users.noreply.github.com>
2020-08-04 13:29:52 +0400

    Добавлена проверка корректности отображения внесенных изменений в группу

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase {
@@ -19,10 +20,15 @@ public class GroupModificationTest extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("edit1", "headerEdit1", "FooterEdit1"));
+    GroupData editGroup = new GroupData(before.get(before.size()-1).getId(),"edit1", "headerEdit1", "FooterEdit1");
+    app.getGroupHelper().fillGroupForm(editGroup);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size()-1);
+    before.add(editGroup);
+    Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
   }
 }
